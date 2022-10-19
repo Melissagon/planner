b9707ca707cc9daeb1bfde33d639308912b4f947
Melissagon <110285166+Melissagon@users.noreply.github.com>
2022-10-18 23:23:34 -0400

    added js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 
+var today = moment().format('dddd, MMMM Do');
+$('#currentDay').text(today);
+$(".saveBtn").on("click", function(){
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
 
-console.log(moment().format("dddd, MMMM Do"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
