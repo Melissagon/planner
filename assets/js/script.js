0b55e59509a4d1acdbbd940c0c60b0b763a746f0
Melissagon <110285166+Melissagon@users.noreply.github.com>
2022-10-19 22:01:43 -0400

    tried to get the message to display when clicking save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 
 var today = moment().format('dddd, MMMM Do');
+var msg = 'Appointment Added to Local Storage'
+
 $('#currentDay').text(today);
+
 $(".saveBtn").on("click", function(){
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
@@ -11,8 +14,15 @@ $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
 $("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
+
+
+setTimeout(function () {
+    document.getElementById("alarmmsg").innerHTML = msg;
+}, 3000);
+
+document.getElementById("alarmmsg").innerHTML = ""; 
